bf713cb56fa658ade17c1bb3584b92c85a97e79d
Danilo Santos <danilo.aparecido.santos@gmail.com>
2023-07-29 08:17:31 -0300

    api para app react

--- src/index.php
+++ src/index.php
@@ -94,4 +94,77 @@ $app->get('/excluir/{id}', function (Request $request, Response $response, $args
     return $response->withHeader('Location', '/')->withStatus(302);
 });
 
+
+// =============== API ===============
+
+// Add routes
+$app->get('/api/caixas', function (Request $request, Response $response) use ($renderer) {
+    // Get the 'tipo' parameter from the query string
+    $params = $request->getQueryParams();
+    $tipo = isset($params['tipo']) ? $params['tipo'] : null;
+
+    // Filter the data based on the 'tipo' parameter
+    $query = Caixa::query();
+    if ($tipo !== null) {
+        $query->where('tipo', 'like', '%' . $tipo . '%');
+    }
+
+    // Fetch all rows from the 'caixas' table
+    $extrato = $query->get();
+
+    // Calculate the total income (where status is 1)
+    $receitas = $extrato->where('status', 1)->sum('valor');
+
+    // Calculate the total expenses (where status is 0)
+    $despesas = $extrato->where('status', 0)->sum('valor');
+
+    // Calculate the total value
+    $valor_total = $receitas - $despesas;
+
+    $payload = [
+        'receitas' => $receitas,
+        'despesas' => $despesas,
+        'valor_total' => $valor_total,
+        'extrato' => $extrato->toArray() // Convert the Eloquent collection to an array
+    ];
+    return $response->withJson($payload);
+});
+
+// Add routes
+$app->post('/api/caixas', function (Request $request, Response $response) {
+    // Get the form data
+    $data = $request->getParsedBody();
+
+    // Create a new 'caixa' record
+    $caixa = new Caixa;
+    $caixa->tipo = $data['tipo'];
+    $caixa->valor = $data['valor'];
+    $caixa->status = $data['status'];
+    $caixa->save();
+
+    return $response->withJson($caixa);
+});
+
+$app->delete('/api/caixas/{id}', function (Request $request, Response $response, $args) {
+    // Get the ID from the route parameters
+    $id = $args['id'];
+    
+    // Find the record in the 'caixas' table and delete it
+    $caixa = Caixa::find($id);
+    if ($caixa) {
+        $caixa->delete();
+    }
+    
+    return $response->withStatus(204);
+});
+
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->run();
